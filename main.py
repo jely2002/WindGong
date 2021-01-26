132d46dedff2d728932376ceabfaddad956989b3
Jelle Glebbeek <jw.glebbeek@gmail.com>
2021-01-26 10:41:23 +0100

    Update to new code that actually does wait

--- main.py
+++ main.py
@@ -1,71 +1,110 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED, Button
 from RpiMotorLib import RpiMotorLib
+import RPi.GPIO as GPIO
+
 import random
 import time
 
-#27 Red RGB pin
-#22 Green RGB pin
-#14 Red button
-#15 Green button
-#4 Blue button (start)
+game_over = False
+waiting_on_press = True
+waiting_on_release = False
+press_green_button = True
 
-def setup():
-    GPIO.setup(22, GPIO.OUT)
-    GPIO.setup(27, GPIO.OUT)
-    GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    motor = RpiMotorLib.BYJMotor("GongMotor", "28BYJ")
-    return motor
 
-def light_green():
-    GPIO.output(22, GPIO.HIGH)
-    GPIO.output(27, GPIO.LOW)
+# 27 Red RGB pin
+# 22 Green RGB pin
+# 14 Red button
+# 15 Green button
+# 4 Blue button (start)
 
-def light_red():
-    GPIO.output(22, GPIO.LOW)
-    GPIO.output(27, GPIO.HIGH)
-    
-def is_green_button_pressed():
-    return GPIO.input(15) 
-    
-def is_red_button_pressed():
-    return GPIO.input(14)
-    
-def wait(condition, motor, granularity=0.1, time_factory=time):
-    end_time = time.time() + random.randint(2,5)  
-    status = condition()         
-    while not status and time.time() < end_time
-        if is_red_button_pressed and not is_green_button_pressed:
-            motor.motor_run([18, 23, 24, 25], 0.001, 64, False, "half", .05)
-            #Draai motor naar rechts/clockwise
-        elif is_green_button_pressed and not is_red_button_pressed:
-            motor.motor_run([18, 23, 24, 25], 0.001, 64, True, "half", .05)
-            #Draai motor naar links/counterclockwise
-        time.sleep(granularity)
-        status = condition()
-    return status  
+def setup_motor():
+    stepper = RpiMotorLib.BYJMotor("motor", "28BYJ")
+    return stepper
+
+
+def light_green(red, green):
+    red.off()
+    green.on()
+
+
+def light_red(red, green):
+    red.on()
+    green.off()
+
+
+def stop_game(red, green):
+    red.off()
+    green.off()
+    GPIO.cleanup()
+
+
+def green_activated():
+    global waiting_on_press
+    global game_over
+    if waiting_on_press and press_green_button:
+        waiting_on_press = not waiting_on_press
+    elif not press_green_button:
+        game_over = True
+
+
+def green_held(stepper):
+    global waiting_on_release
+    if waiting_on_release and press_green_button:
+        stepper.motor_run([18, 23, 24, 25], .01, 100, False, False, "half", .05)
+
+
+def red_activated():
+    global waiting_on_press
+    global game_over
+    if waiting_on_press and not press_green_button:
+        waiting_on_press = not waiting_on_press
+    elif press_green_button:
+        game_over = True
+
+
+def red_held(stepper):
+    global waiting_on_release
+    if waiting_on_release and not press_green_button:
+        stepper.motor_run([18, 23, 24, 25], .01, 100, False, False, "half", .05)
+
 
 if __name__ == '__main__':
-    motor = setup()
-    game_over = False
-    press_green_button = True
-    wait_time = random.randint(2,5)
+    red_led = LED(27)
+    green_led = LED(22)
+    red_button = Button(14)
+    green_button = Button(15)
+    blue_button = Button(4)
+    motor = setup_motor()
+
+    green_button.when_activated(green_activated())
+    green_button.when_held(green_held(motor))
+    red_button.when_activated(red_activated())
+    red_button.when_held(red_held(motor))
+
+    global waiting_on_release
+    global waiting_on_press
+    global game_over
+    global press_green_button
+
     while not game_over:
-        if press_green_button: #Groene knop moet ingedrukt worden
-            if wait(is_green_button_pressed, motor):
-                press_green_button = not press_green_button
-                light_red()
-            else:
-                game_over = True
+        if press_green_button:
+            waiting_on_press = True
+            green_button.wait_for_active(1)
+            if waiting_on_press or game_over:
                 break
-        else: #Rode knop moet ingedrukt worden
-            if wait(is_red_button_pressed, motor):
-                press_green_button = not press_green_button
-                light_green()
-            else:
-                game_over = True
+            waiting_on_release = True
+            time.sleep(random.randint(2, 5))
+            light_red(red_led, green_led)
+            waiting_on_release = False
+            press_green_button = False
+        else:
+            waiting_on_press = True
+            red_button.wait_for_active(1)
+            if waiting_on_press or game_over:
                 break
-    GPIO.cleanup()
-            
-        
+            waiting_on_release = True
+            time.sleep(random.randint(2, 5))
+            light_green(red_led, green_led)
+            waiting_on_release = False
+            press_green_button = True
+    stop_game(red_led, green_led)
